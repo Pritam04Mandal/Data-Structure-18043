386196c4d715518754420937ea8e884870f17f8c
Pritam Mandal <103989209+Pritam04Mandal@users.noreply.github.com>
2022-10-07 11:26:20 +0530

    Update bst.cpp

--- bst.cpp
+++ bst.cpp
@@ -11,8 +11,9 @@ class BST{
                 int data;
 
         };
-        node *root,*p,*q;
+        node *p=nullptr,*q=nullptr;
     public:
+    node *root=nullptr;
         void insert()
         {
             int i;
@@ -31,22 +32,25 @@ class BST{
                     q=new node();
                     q->left=q->right=nullptr;
                     q->data=i;
+                    node *p1;
                     while(p!=nullptr)
                     {
                         if(p->data>i)
                         {
-                            p=p->right;
-                        }
-                        else{
+                            p1=p;
                             p=p->left;
                         }
+                        else{
+                            p1=p;
+                            p=p->right;
+                        }
                     }
-                    if(i>p->data)
+                    if(i>p1->data)
                     {
-                        p->right=q;
+                        p1->right=q;
                     }
                     else{
-                        p->left=q;
+                        p1->left=q;
                     }
                 }
                 cout<<"Enter the node value(Enter 0 to exit): ";
@@ -77,10 +81,80 @@ class BST{
         {
             if(temp!=nullptr)
             {
-                inorder_rec(temp->left);
-                inorder_rec(temp->right);
+                postorder_rec(temp->left);
+                postorder_rec(temp->right);
                 cout<<temp->data<<" ";
             }
+        }
+        int leftmost(node *temp,int counter)
+        {
+            if(temp!=nullptr)
+            {
+                int i=leftmost(temp->left,counter+1);
+            }
+            else{
+                return counter;
+            }
+        }
+        int rightmost(node *temp,int counter)
+        {
+            if(temp!=nullptr)
+            {
+                int i=rightmost(temp->left,counter+1);
+            }
+            else{
+                return counter;
+            }
+        }
+        int height(node *temp)
+        {
+            int hgtr=0,hgtl=0;
+            while(temp->left!=nullptr||temp->right!=nullptr)
+            {
+                if(temp->left!=nullptr)
+                {
+                    temp=temp->left;
+                    hgtr=hgtr+1;
+                }
+                if(temp->right!=nullptr)
+                {
+                    temp=temp->right;
+                    hgtr=hgtr+1;
+                }
+            }
+            temp=root;
+            while(temp->left!=nullptr||temp->right!=nullptr)
+            {
+                if(temp->right!=nullptr)
+                {
+                    temp=temp->right;
+                    hgtl=hgtl+1;
+                }
+                if(temp->left!=nullptr)
+                {
+                    temp=temp->left;
+                    hgtl=hgtl+1;
+                }
+            }
+            int lft=leftmost(root,-1);
+            int rgt=rightmost(root,-1);
+            cout<<hgtl<<" "<<hgtr<<" "<<lft<<" "<<rgt<<endl;
+            if(lft>=rgt && lft>=hgtl && lft>=hgtr)
+            {
+                return lft;
+            }
+            else if(rgt>=lft && rgt>=hgtl && rgt>=hgtr)
+            {
+                return rgt;
+            }
+            else if(hgtr>=rgt && hgtr>=hgtl && hgtr>=lft)
+            {
+                return hgtr;
+            }
+            else{
+                return hgtl;
+            }
+            
         }
         void preorder_itr();
         void inorder_itr();
@@ -93,6 +167,16 @@ class BST{
 
 int main()
 {
+    BST b;
+
+    b.insert();
+    cout<<b.height(b.root)<<endl;
+    b.preorder_rec(b.root);
+    cout<<endl;
+    b.inorder_rec(b.root);
+    cout<<endl;
+    b.postorder_rec(b.root);
+    cout<<endl;
 
 return 0;
 }
